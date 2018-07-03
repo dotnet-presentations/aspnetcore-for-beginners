ce5c3f0650f0cd13fb776b8950baf2ab6d57f619
Jasmine G <paladique@users.noreply.github.com>
2018-07-03 15:28:38 -0400

    update to 2.1

--- Final_Project/RazorPagesMovie/Startup.cs
+++ Final_Project/RazorPagesMovie/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace RazorPagesMovie
 {
@@ -23,8 +26,16 @@ namespace RazorPagesMovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
             services.AddDbContext<MovieContext>(options => options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
-            services.AddMvc();
+            services.AddMvc()
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);       
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,9 +48,12 @@ namespace RazorPagesMovie
             else
             {
                 app.UseExceptionHandler("/Error");
+                app.UseHsts();
             }
 
             app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
