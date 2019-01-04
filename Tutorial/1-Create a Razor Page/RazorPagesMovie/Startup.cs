b2ce1d71cda93424b3426325c81c9d864c3d20eb
LadyNaggaga <maria.naggaga@live.ca>
2019-01-04 13:00:03 -0500

    Updated Tutorial 1 in Vs Code  - to ASP.NET Core 2.2

--- Tutorial/1-Create a Razor Page/RazorPagesMovie/Startup.cs
+++ Tutorial/1-Create a Razor Page/RazorPagesMovie/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
